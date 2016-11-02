8bbe9dd12283910a0589aceb4c23e40f09b2a0fb
danilolosi <danilo.lsanctis@hotmail.com>
2016-11-02 18:55:39 -0200

    Adicionando nova Solution e fazendo CRUD simples

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
@@ -0,0 +1,37 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContext _context = new PortalContext();
+
+        // GET: Aluno
+        public ActionResult Cadastrar()
+        {
+            return View("Cadastrar");
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno a)
+        {
+            TempData["msg"] = "Cadastrado com sucesso";
+            _context.Aluno.Add(a);
+            _context.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+          List<Aluno> lista = _context.Aluno.ToList();
+
+            return View(lista);
+        }
+    }
+}
